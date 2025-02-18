06ecf3ce898eff6389d2ae48e56ef4c95a436e54
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-18 13:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+function find(element)
+{
+	let level = 0;
+
+	while(element)
+		{
+			level++;
+			element = element.parentElement;
+		}
+	return level;
+}
+
+
+let element = document.getElementById("level");
+
+ alert("The level of the element is: "+find(element));
